18ebda417d5e3bec45d193769283e6530869dd70
Aleksandr Sirchenko <sirchik@gmail.com>
2016-02-15 17:05:29 +0000

    database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['Customer', 'Manager', 'Admin', 'Banned'].each do |role|
+  Role.find_or_create_by({name: role})
+end
+
+['USA', 'Ukraine', 'Poland', 'Canada'].each do |country|
+  Country.find_or_create_by({name: country})
+end
+
+User.find_or_create_by({email: 'admin@amazonchik.com'}) do |user|
+  user.firstname = 'Admin'
+  user.lastname = 'Admin'
+  user.role_id = Role.find_by_name('Admin')
+  user.password = '$2a$10$wzHfYwOrbFhwh2ZPnH48VeGlDpSZPVCNt9328eYDeZ4lPIziSJ9TS' #AdminAdmin
+end
